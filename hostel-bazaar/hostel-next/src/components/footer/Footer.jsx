5656b933c036cc66563a9b8726d358e2e78d7592
Shahnawaz Alam <91892524+snawaza243@users.noreply.github.com>
2023-06-18 22:46:18 +0530

    first commit

---
+++ hostel-bazaar/hostel-next/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className="">© 2023 Next. All rights reserved.</div>
+        <div className=""></div>
+    </div>
+  )
+}
+
+export default Footer
